eccace8bed552c0848583892b2f153cf4cb917a6
fady <fadyyoussef4444@gamil.com>
2025-09-10 16:44:22 +0300

    feat: create inputs for rest controller

---
+++ src/main/java/com/example/book_store/payload/LoginInput.java
@@ -0,0 +1,16 @@
+package com.example.book_store.payload;
+
+import com.example.book_store.validation.CustomNotNull;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+
+public record LoginInput(
+        @CustomNotNull(fieldName = "email")
+        @Email(message = "Email is not valid")
+        String email,
+        @CustomNotNull(fieldName = "password")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+                message = "password is not valid")
+        String password
+) {
+}
