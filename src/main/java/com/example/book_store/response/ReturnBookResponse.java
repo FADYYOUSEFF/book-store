d28dcb927dfb6ac589a82200e096ac1c2727df5e
fady <fadyyoussef4444@gamil.com>
2025-09-10 16:46:11 +0300

    feat: create response for borrow feature to seperate the borrow entity and hide the user details from the client

---
+++ src/main/java/com/example/book_store/response/ReturnBookResponse.java
@@ -0,0 +1,14 @@
+package com.example.book_store.response;
+
+import com.example.book_store.entity.BookCopy;
+import com.example.book_store.enums.BorrowingStatus;
+
+import java.util.UUID;
+
+public record ReturnBookResponse(
+        UUID borrowingId,
+        String userEmail,
+        BookCopy bookCopy,
+        BorrowingStatus borrowingStatus
+) {
+}
